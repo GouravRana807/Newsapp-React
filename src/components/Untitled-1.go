69f3b0f3243ef07bb01742771b1af9a0d058463a
Gourav Rana <gouravrana870@gmail.com>
2022-03-22 20:07:22 +0530

    Intial commit

---
+++ src/components/Untitled-1.go
@@ -0,0 +1,31 @@
+<Route path="/business">
+              <News pageSize={5} country="in" category="general" />
+            </Route>
+<Route path="/entertainment">
+              <News pageSize={5} country="in" category="general" />
+            </Route>
+<Route path="/general">
+              <News pageSize={5} country="in" category="general" />
+            </Route>
+<Route path="/health">
+              <News pageSize={5} country="in" category="general" />
+            </Route>
+<Route path="/science">
+              <News pageSize={5} country="in" category="general" />
+            </Route>
+<Route path="/sports">
+              <News pageSize={5} country="in" category="general" />
+            </Route>
+<Route path="/technology">
+              <News pageSize={5} country="in" category="general" />
+            </Route>
+
+
+
+
+
+
+
+ <Route path="/">
+              <News pageSize={5} country="in" category="general" />
+            </Route>
